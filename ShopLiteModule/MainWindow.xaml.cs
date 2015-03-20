41d274464ab32912b0eb9091288dcc6eeae87e74
Alex Yang <gya10@sfu.ca>
2015-03-19 19:39:02 -0700

    Finished BackgroundWorker framework setup + a few UI changes

--- ShopLiteModule/MainWindow.xaml.cs
+++ ShopLiteModule/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data;
+using System.Threading;
+using System.ComponentModel;
+using System.Globalization;
 
 namespace ShopLiteModule
 {
@@ -22,18 +25,29 @@ namespace ShopLiteModule
     public partial class MainWindow : Window
     {
         private DBConnection con;
+        private BackgroundWorker worker;
+
         public MainWindow()
         {
             InitializeComponent();
             initImage();
             initDB();
-            refreshList();
+            initBgWorker();
         }
 
-        private void CheckoutBtn_clicked(object sender, RoutedEventArgs e)
+        private void checkoutBtnClicked(object sender, RoutedEventArgs e)
         {
         }
 
+        private void rescanBtnClicked(object sender, RoutedEventArgs e)
+        {
+            initBgWorker();
+        }
+
+        private void cancelBtnClicked(object sender, RoutedEventArgs e)
+        {
+            worker.CancelAsync();
+        }
         private void AskforassistBtn_clicked(object sender, RoutedEventArgs e)
         {
             refreshList();
@@ -41,7 +55,8 @@ namespace ShopLiteModule
 
         private void initDB()
         {
-            con = new DBConnection();   
+            con = new DBConnection();
+            refreshList();
         }
 
         private void initImage()
@@ -56,15 +71,81 @@ namespace ShopLiteModule
             //Console.Out.WriteLine(data.Rows[0]["Price"]);
         }
 
+        private void refreshList(DataTable data)
+        {
+            myList.ItemsSource = null;
+            myList.ItemsSource = data.DefaultView;
+            TotalPriceLbl.Content = calculateTotalPrice(data);
+        }
+
+        private void initBgWorker(){
+            worker = new BackgroundWorker();
+
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += _workerDoWork;
+            worker.ProgressChanged += _workerProgressChanged;
+            worker.RunWorkerCompleted += _workerJobComplete;
+
+            TimerStatusLbl.Content = "Scanning...";
+            cancelBtn.Visibility = Visibility.Visible;
+
+            worker.RunWorkerAsync();
+        }
+
         private string calculateTotalPrice(DataTable data) { 
             string output = "";
             double sum = 0.0d;
             foreach(DataRow row in data.Rows){
                 sum += Double.Parse(System.Convert.ToString(row["Price"]));
             }
-            output = System.Convert.ToString(sum);
+            output = sum.ToString("C2", CultureInfo.CurrentCulture);
             return output;
         }
 
+        private void _workerDoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            for (int i = 0; i < 100; i++)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    // TODO:
+                    // use Reader.dll to scan tags
+                    // check db with serialIDs
+                    // refresh the list: refreshList(DataTable data)
+                    worker.ReportProgress(i);
+                    Thread.Sleep(100);
+                }
+            }
+        }
+        private void _workerJobComplete(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                TimerStatusLbl.Content = "Scanning cancelled!";
+                Timer.Value = 0;
+            }
+            else if (!(e.Error == null))
+            {
+                //TODO
+            }
+            else
+            {
+                TimerStatusLbl.Content = "Finished scanning.";
+            }
+
+            cancelBtn.Visibility = Visibility.Hidden;
+        }
+
+        private void _workerProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Timer.Value = e.ProgressPercentage;
+        }
     }
 }
