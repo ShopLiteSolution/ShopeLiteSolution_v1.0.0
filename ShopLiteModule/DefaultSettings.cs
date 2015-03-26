58117c2307e88c9414c64cb91c4cc4c8aa1de23d
Alex Yang <gya10@sfu.ca>
2015-03-26 16:27:09 -0700

    Start to integerate the code

---
+++ ShopLiteModule/DefaultSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopLiteModule
+{
+    class DefaultSettings
+    {
+        //Scale connection
+        public static string Default_COM = "COM1";
+        public static string Default_Baudrate = "115200";
+        
+    }
+}
